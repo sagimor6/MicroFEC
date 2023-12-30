f5b03e095e094cf49b3a7f04697f9c155367f2c3
Sagi Mor <sagimor6@gmail.com>
2023-12-30 22:03:05 +0200

    perf and test

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 
 CC = gcc
+OBJCOPY = objcopy
 
 CFLAGS += -pedantic-errors -Wno-pedantic -Wall -Wextra -Wnull-dereference -Waggressive-loop-optimizations -Wstrict-overflow=5 -Wuninitialized
 CFLAGS += -DFEC_MIN_MEM -DFEC_LARGE_K
 
-DEBUG_CFLAGS = -fsanitize=undefined -fsanitize=address -g
+DEBUG_CFLAGS += -g
+#DEBUG_CFLAGS += -fsanitize=undefined -fsanitize=address 
 
-OPT_CFLAGS = -flto -ffat-lto-objects -ffunction-sections -fdata-sections -Os -fvisibility=hidden -s
+OPT_CFLAGS += -flto -ffat-lto-objects -ffunction-sections -fdata-sections -Os -fvisibility=hidden -s
+OPT_CFLAGS += -mpclmul
 OPT_LDFLAGS = -Wl,--gc-sections
 
 BUILD = build
@@ -16,13 +19,13 @@ all: $(BUILD)/libmicro_fec.so $(BUILD)/libmicro_fec.a fec_test
 
 .PHONY fec_test:
 fec_test: $(BUILD)/fec_test.elf
-	$(BUILD)/fec_test.elf
+	$(BUILD)/fec_test.elf 1000 100 1400
 
 $(BUILD)/:
 	mkdir -p $@
 
 $(BUILD)/fec_test.elf: main.c micro_fec.c | $(BUILD)/
-	$(CC) $(CFLAGS) $(DEBUG_CFLAGS) -o $@ $^
+	$(CC) $(CFLAGS) $(DEBUG_CFLAGS) $(OPT_CFLAGS) $(OPT_LDFLAGS) -o $@ $^
 
 $(BUILD)/micro_fec.o_static: micro_fec.c | $(BUILD)/
 	$(CC) $(CFLAGS) $(OPT_CFLAGS) -c -o $@ $^
--- main.c
+++ main.c
@@ -1,4 +1,9 @@
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 #include "micro_fec.h"
 
@@ -14,74 +19,203 @@
     TRACE("\n");\
 }
 
-#define CHECK(cond) if(!(cond)) { TRACE("check failed %d %s\n", __LINE__, #cond); }
+#define CHECK(cond) if(!(cond)) { TRACE("check failed %d %s\n", __LINE__, #cond); goto cleanup; }
 
-int main(void) {
 
+unsigned int rand_range(unsigned int n) {
+    if (n > ((unsigned int)RAND_MAX + 1)) {
+        abort();
+    }
+
+    unsigned int max = RAND_MAX - (((unsigned int)RAND_MAX + 1) % n);
+
+    while (true) {
+        unsigned int val = rand();
+        if (val <= max) {
+            return (val % n);
+        }
+    }
+}
+
+// TODO: this is can be done with O(k) mem with dict.
+unsigned int* rand_unique_indexes(unsigned int n, unsigned int k) {
+    unsigned int* arr = malloc(n*sizeof(unsigned int));
+    unsigned int i;
+
+    if (arr == NULL) {
+        return NULL;
+    }
+
+    for (i = 0; i < n; i++) {
+        arr[i] = i;
+    }
+
+    for (i = 0; i < k; i++) {
+        unsigned int j = rand_range(n - i) + i;
+        unsigned int tmp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = tmp;
+    }
+
+    unsigned int* arr2 = realloc(arr, k*sizeof(unsigned int));
+    if (arr2 == NULL) {
+        return arr; // weird but ok
+    }
+
+    return arr2;
+}
+
+void rand_fill(void* buf, size_t len) {
+    size_t i;
+    for (i = 0; i < len; i++) {
+        // TODO: inefficient but idc
+        ((uint8_t*)buf)[i] = (uint8_t)rand_range(1<<8);
+    }
+}
+
+uint64_t get_timestamp() {
+    struct timespec tp = {0};
+    //CHECK(clock_gettime(CLOCK_MONOTONIC, &tp) == 0);
+    CHECK(clock_gettime(CLOCK_THREAD_CPUTIME_ID, &tp) == 0);
+
+cleanup:
+    return (tp.tv_sec*1000000000ULL) + tp.tv_nsec;
+}
+
+void test_perf(unsigned int n, unsigned int k, unsigned int pak_len) {
+    // const unsigned int n = 1000;
+    // const unsigned int k = 100;
+    // const size_t pak_len = 1400 / sizeof(uint16_t);
+    fec_int_t *paks = NULL;
+    fec_int_t *r_paks = NULL;
+    unsigned int *rcv_idxs = NULL;
+    unsigned int i;
+    bool inited_state = false;
     fec_state_t state;
+    bool inited_tx_state = false;
     fec_tx_state_t tx_state;
+    bool inited_rx_state = false;
     fec_rx_state_t rx_state;
+    uint64_t start_time, end_time;
 
-    unsigned int i, j;
+    CHECK((pak_len % sizeof(fec_int_t)) == 0);
 
-    uint16_t paks[3][2] = {{1, 4}, {2, 5}, {3, 6}};
-    uint16_t r_paks[3][2];
+    pak_len /= sizeof(fec_int_t);
+
+    paks = malloc(n * pak_len * sizeof(uint16_t));
+    CHECK(paks != NULL);
+
+    r_paks = malloc(k * pak_len * sizeof(uint16_t));;
+    CHECK(r_paks != NULL);
+
+    srand(1337/*time(NULL)*/);
+
+    rcv_idxs = rand_unique_indexes(n + k, n);
+    CHECK(rcv_idxs != NULL);
+
+    rand_fill(paks, n * pak_len * sizeof(uint16_t));
 
     TRACE("--0--\n");
+    start_time = get_timestamp();
 
-    fec_init(&state, sizeof(paks)/sizeof(paks[0]), sizeof(r_paks)/sizeof(r_paks[0]), sizeof(paks[0])/sizeof(paks[0][0]));
-    fec_tx_init(&tx_state, &state);
-    fec_rx_init(&rx_state, &state);
+    CHECK(fec_init(&state, n, k, pak_len));
+    inited_state = true;
+    CHECK(fec_tx_init(&tx_state, &state));
+    inited_tx_state = true;
+    CHECK(fec_rx_init(&rx_state, &state));
+    inited_rx_state = true;
 
-    TRACE("--1--\n");
+    end_time = get_timestamp();
+    TRACE("--total init time-- %f\n", (end_time - start_time) / ((double)1000000000));
+    start_time = get_timestamp();
 
-    for (i = 0; i < sizeof(paks)/sizeof(paks[0]); i++) {
-        fec_tx_add_info_pak(&tx_state, paks[i], i);
+    for (i = 0; i < n; i++) {
+        fec_tx_add_info_pak(&tx_state, &paks[i*pak_len], i);
     }
 
-    TRACE("--2--\n");
+    end_time = get_timestamp();
+    TRACE("--fec_tx_add_info_pak-- %f\n", (end_time - start_time) / ((double)1000000000));
+    start_time = get_timestamp();
 
-    for (i = 0; i < sizeof(r_paks)/sizeof(r_paks[0]); i++) {
-        fec_tx_get_redundancy_pak(&tx_state, i, r_paks[i]);
+    for (i = 0; i < k; i++) {
+        fec_tx_get_redundancy_pak(&tx_state, i, &r_paks[i*pak_len]);
     }
 
-    TRACE("--3--\n");
+    end_time = get_timestamp();
+    TRACE("--fec_tx_get_redundancy_pak-- %f\n", (end_time - start_time) / ((double)1000000000));
+    start_time = get_timestamp();
 
-    for (i = 0; i < sizeof(paks)/sizeof(paks[0]); i++) {
-        if (i == 0 || i == 2 || i == 1) {
-            continue;
+    for (i = 0; i < n; i++) {
+        unsigned int idx = rcv_idxs[i];
+        bool can_recover;
+        bool can_discard;
+        
+        if (idx < n) {
+            CHECK(fec_rx_add_pak(&rx_state, &paks[idx*pak_len], idx, &can_recover, &can_discard));
+        } else {
+            CHECK(fec_rx_add_pak(&rx_state, &r_paks[(idx - n)*pak_len], idx, &can_recover, &can_discard));
         }
-        fec_rx_add_pak(&rx_state, paks[i], i, NULL, NULL);
+        CHECK(can_recover == (i == n - 1));
+        CHECK(!can_discard);
+        
+        if (idx < n) {
+            CHECK(fec_rx_add_pak(&rx_state, &paks[idx*pak_len], idx, &can_recover, &can_discard));
+        } else {
+            CHECK(fec_rx_add_pak(&rx_state, &r_paks[(idx - n)*pak_len], idx, &can_recover, &can_discard));
+        }
+        CHECK(can_recover == (i == n - 1));
+        CHECK(can_discard);
     }
 
-    TRACE("--4--\n");
+    end_time = get_timestamp();
+    TRACE("--fec_rx_add_pak-- %f\n", (end_time - start_time) / ((double)1000000000));
+    start_time = get_timestamp();
 
-    for (i = 0; i < sizeof(r_paks)/sizeof(r_paks[0]); i++) {
-        fec_rx_add_pak(&rx_state, r_paks[i], (sizeof(paks)/sizeof(paks[0])) + i, NULL, NULL);
-    }
+    CHECK(fec_rx_fill_missing_paks(&rx_state));
 
-    TRACE("--5--\n");
-
-    if (!fec_rx_fill_missing_paks(&rx_state)) {
-        return 0;
-    }
-
-    TRACE("--6--\n");
+    end_time = get_timestamp();
+    TRACE("--fec_rx_fill_missing_paks-- %f\n", (end_time - start_time) / ((double)1000000000));
+    start_time = get_timestamp();
 
     uint16_t** res = (uint16_t**)fec_rx_get_info_paks(&rx_state);
 
-    for (j = 0; j < sizeof(paks[0])/sizeof(paks[0][0]); j++) {
-        for (i = 0; i < sizeof(paks)/sizeof(paks[0]); i++) {
-            TRACE("%d ", res[i][j]);
+    for (i = 0; i < n; i++) {
+        if (res[i] == &paks[i*pak_len]) {
+            continue;
         }
-        TRACE("\n");
+        CHECK(memcmp(res[i], &paks[i*pak_len], pak_len * sizeof(uint16_t)) == 0);
     }
 
-    fec_rx_reset(&rx_state); // not neede here, but to test
+cleanup:
+    if (paks != NULL) {
+        free(paks);
+    }
+    if (r_paks != NULL) {
+        free(r_paks);
+    }
+    if (rcv_idxs != NULL) {
+        free(rcv_idxs);
+    }
 
-    fec_rx_destroy(&rx_state);
-    fec_tx_destroy(&tx_state);
-    fec_destroy(&state);
+    if (inited_rx_state) {
+        fec_rx_destroy(&rx_state);
+    }
 
+    if (inited_tx_state) {
+        fec_tx_destroy(&tx_state);
+    }
+
+    if (inited_state) {
+        fec_destroy(&state);
+    }
+}
+
+int main(int argc, const char* argv[]) {
+    if (argc != 4) {
+        TRACE("usage: %s N K PAK_LEN\n", argc > 0 ? argv[0] : "./program");
+        return 0;
+    }
+
+    test_perf(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
     return 0;
 }
