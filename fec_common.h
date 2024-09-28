3dfbbc48e419673b268e76b1638498e822b8914e
Sagi Mor <sagimor6@gmail.com>
2024-09-28 18:03:42 +0300

    perf refactor, no more code dup, forgot

---
+++ fec_common.h
@@ -0,0 +1,77 @@
+#ifndef __FEC_COMMON_H__
+#define __FEC_COMMON_H__
+
+#include <stdint.h>
+
+#include "micro_fec.h"
+
+#ifdef PERF_DEBUG
+#define PERF_DEBUG_ATTRS __attribute__((noinline)) __attribute__((aligned(16)))
+#else
+#define PERF_DEBUG_ATTRS 
+#endif
+
+#ifndef MAX
+#define MAX(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+#endif
+
+#ifndef MIN
+#define MIN(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+#endif
+
+#define CALC_EXTRA_FOR_ALIGNED_MALLOC(typ) (__alignof__(typ) > __alignof__(max_align_t) ? __alignof__(typ) - __alignof__(max_align_t) : 0)
+
+#define POLY_G ((fec_int_t)0b0000000000101011)
+
+#if defined(FEC_HAS_64_INT_VEC) || defined (FEC_HAS_CLMUL32) || 1
+
+typedef uint8_t u8x16 __attribute__ ((vector_size (16)));
+typedef uint16_t u16x8 __attribute__ ((vector_size (16)));
+typedef uint32_t  u32x4 __attribute__ ((vector_size (16)));
+typedef uint64_t  u64x2 __attribute__ ((vector_size (16)));
+
+typedef uint8_t u8x32 __attribute__ ((vector_size (32)));
+typedef uint16_t u16x16 __attribute__ ((vector_size (32)));
+typedef uint32_t  u32x8 __attribute__ ((vector_size (32)));
+typedef uint64_t  u64x4 __attribute__ ((vector_size (32)));
+
+typedef int16_t i16x16 __attribute__ ((vector_size (32)));
+typedef int16_t i16x8 __attribute__ ((vector_size (16)));
+
+typedef uint8_t u8x8 __attribute__ ((vector_size (8)));
+typedef uint16_t u16x4 __attribute__ ((vector_size (8)));
+typedef uint32_t  u32x2 __attribute__ ((vector_size (8)));
+typedef uint64_t  u64x1 __attribute__ ((vector_size (8)));
+
+#if (defined(__x86_64__) || defined (__i386__))
+#include <immintrin.h>
+#elif defined(__arm__) || defined(__aarch64__)
+#include <arm_neon.h>
+#endif
+
+#if defined(__AVX2__)
+#define my_mm256_extracti128_si256 _mm256_extracti128_si256
+#elif defined(__AVX__)
+#define my_mm256_extracti128_si256 _mm256_extractf128_si256
+#else
+#define my_mm256_extracti128_si256(x, y) ((u64x2){((u64x4)x)[y*2+0], ((u64x4)x)[y*2+1]})
+#endif
+
+#if defined(__SSE2__)
+#define my_mm_shuffle_epi32(x, a, b, c, d) _mm_shuffle_epi32(x, _MM_SHUFFLE(d, c, b, a))
+#elif defined(__SSE__)
+#define my_mm_shuffle_epi32(x, a, b, c, d) ((u32x4)_mm_shuffle_ps(x, x, _MM_SHUFFLE(d, c, b, a)))
+#else
+#define my_mm_shuffle_epi32(x, a, b, c, d) ((u32x4){((u32x4)x)[a], ((u32x4)x)[b], ((u32x4)x)[c], ((u32x4)x)[d]})
+#endif
+
+#endif
+
+
+#endif // __FEC_COMMON_H__
---
+++ opt_templates/dummy_defs.h
@@ -0,0 +1,35 @@
+
+
+#ifndef FMA_FUNC_NAME
+#define FMA_FUNC_NAME fec_col_fma
+#endif
+
+#ifndef NORM_FUNC_NAME
+#define NORM_FUNC_NAME fec_norm
+#endif
+
+#ifndef INIT_FUNC_NAME
+#define INIT_FUNC_NAME fec_init_col
+#endif
+
+#ifndef LEN_PARAM_TYPE
+#define LEN_PARAM_TYPE size_t
+#endif
+
+#ifndef INPUT_ARGS
+#define INPUT_ARGS const fec_int_t *__dummy
+#endif
+
+#ifndef OUTPUT_ARGS
+#define OUTPUT_ARGS fec_int_t *__dummy
+#endif
+
+#ifndef READ_INPUT
+#define READ_INPUT(j) __dummy[j]
+#endif
+
+#ifndef WRITE_OUTPUT
+#define WRITE_OUTPUT(j, val) __dummy[j] = val
+#endif
+
+
